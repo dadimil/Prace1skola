d5d78232b73183031f5b043e82861231fb91df6d
KuceraD.02 <KuceraD.02@spst.eu>
2021-05-07 09:07:02 +0200

    druhetlac

--- Prace1skola/Form1.cs
+++ Prace1skola/Form1.cs
@@ -21,5 +21,10 @@ namespace Prace1skola
         {
             MessageBox.Show("Zdravím");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Brý den");
+        }
     }
 }
